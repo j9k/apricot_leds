d61552cb64c556bd4da12f734b5409853c39f840
j9k <jud9000@gmail.com>
2015-02-13 12:55:44 -0800

    interrupt tweaking

--- arduino_led/arduino_led.ino
+++ arduino_led/arduino_led.ino
@@ -14,6 +14,13 @@ volatile uint16_t interruptCount=0;
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800); //  neopixel setup
 
+
+//debounce variables
+long debouncing_time = 15; //Debouncing Time in Milliseconds
+volatile unsigned long last_micros;
+
+
+
 int pause = 1;
 int buttonPin = 10;
 int hddPin = 11;
@@ -21,6 +28,8 @@ int buttonCount = 1;
 int red = 80;
 int green = 80;
 int blue = 80;
+
+
 
 void setup() {
     pixels.begin();
@@ -36,7 +45,7 @@ pinMode(hddPin, INPUT_PULLUP);
 
 void loop() {
   
-  buttonCount = interruptCount % 3;
+  buttonCount = interruptCount % 2;
   
   
   switch (buttonCount) {
@@ -55,9 +64,16 @@ void loop() {
 
 
 
-void button_ISR(){
-interruptCount++;
-pause = pause++ % 100;
+void button_ISR(){        // this function needs to be short and exit naturally
+    if((long)(micros() - last_micros) >= debouncing_time * 1000) {
+      interruptCount++;
+      pause = pause++ % 100;
+    
+    last_micros = micros();
+    }
+  
+  
+
 }
 
 void clear_Led() {
@@ -70,7 +86,7 @@ void clear_Led() {
 
 
 void colorcycle(){
- // pause = 100;
+  pause = interruptCount;
   for(int i=0;i<NUMPIXELS;i++){
     pixels.setPixelColor(i, pixels.Color(red,0,0)); 
     pixels.show(); 
